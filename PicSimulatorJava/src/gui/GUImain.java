8ea32357b9b05d8ccea7b848ebf33fb558064c64
BitCombi <josephineludwigemail@gmail.com>
2025-04-17 13:30:25 +0200

    Pls push??

--- PicSimulatorJava/src/gui/GUImain.java
+++ PicSimulatorJava/src/gui/GUImain.java
@@ -3,6 +3,9 @@ package gui;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 
 public class GUImain {
 
@@ -43,7 +46,15 @@ public class GUImain {
 		shell = new Shell();
 		shell.setSize(450, 300);
 		shell.setText("SWT Application");
+		
+		Button btnNewButton = new Button(shell, SWT.NONE);
+		btnNewButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+			}
+		});
+		btnNewButton.setBounds(71, 58, 216, 30);
+		btnNewButton.setText("TestButton");
 
 	}
-
 }
