72564aa53b9813f73e0ae18859671155427b521a
Jinfei <jinfei.cao2004@gmail.com>
2025-05-17 00:44:50 +0800

    I/O pin->Port,Tris

--- PicSimulatorJava/src/simulatorCode/DataMemory.java
+++ PicSimulatorJava/src/simulatorCode/DataMemory.java
@@ -4,36 +4,68 @@ package simulatorCode;
  * Only RAM-Registers (p.13/14 in DataSheet).
  */
 public class DataMemory {
-    private int[] ram = new int[256];// Addresses: 0x00-0xFF 
+    private int[] ram = new int[256];// Addresses: 0x00-0xFF
 
-    //Addresses of seperate Registers
+    // Addresses of seperate Registers
     public static final int ADDR_TMR0 = 0x01;
     public static final int ADDR_STATUSB0 = 0x03;
     public static final int ADDR_STATUSB1 = 0x83;
     public static final int ADDR_OPTION = 0x81;
-    
-    //Masks to get specific Bit of Register
+
+    // Masks to get specific Bit of Register
     public static final int C_Mask = 0x01;
     public static final int DC_Mask = 0x02;
     public static final int Z_Mask = 0x03;
 
-    /*writes VALUE at ADDRESS(=Register*/
+    private Port portA = new Port("A", 5);
+    private Port portB = new Port("B", 8);
+
+    /* writes VALUE at ADDRESS(=Register */
     public void write(int address, int value) {
-        ram[address] = value & 0xFF;// RAM has 8-bit Numbers only (1111 1111)
+        switch (address) {
+        case 0x05:
+            portA.write(value);
+            break;
+        case 0x06:
+            portB.write(value);
+            break;
+        case 0x85:
+            portA.setTris(value);
+            break;
+        case 0x86:
+            portB.setTris(value);
+            break;
+        default:
+            ram[address] = value & 0xFF;// RAM has 8-bit Numbers only (1111 1111)
+            break;
+        }
 
     }
-    
-    /*reads value at ADDRESS(=Register)*/
+
+    /* reads value at ADDRESS(=Register) */
     public int read(int address) {
-        return ram[address];
+        switch (address) {
+        case 0x05:
+            return portA.read();
+        case 0x06:
+            return portB.read();
+        case 0x85:
+            return portA.getTris();
+        case 0x86:
+            return portB.getTris();
+        default:
+            return ram[address];
+        }
     }
-    
-    // --- PCL --- (Addr: 0x02 & 0x82) -------------------------------------------------
 
-    // --- STATUS --- (Addr: 0x03 & 0x83) ----------------------------------------------
+    // --- PCL --- (Addr: 0x02 & 0x82)
+    // -------------------------------------------------
+
+    // --- STATUS --- (Addr: 0x03 & 0x83)
+    // ----------------------------------------------
     public void setStatusBit(int mask) {
-        int status = read(ADDR_STATUSB0);		//only need to read one, bcs we made sure to always map it !!
-        write(ADDR_STATUSB0, status | mask);	//
+        int status = read(ADDR_STATUSB0); // only need to read one, bcs we made sure to always map it !!
+        write(ADDR_STATUSB0, status | mask); //
         write(ADDR_STATUSB1, status | mask);
     }
 
@@ -50,11 +82,11 @@ public class DataMemory {
             clearStatusBit(Z_Mask);
         }
     }
-    
+
     public void setCarryFlag(boolean value) {
-        if(value) {
-            setStatusBit(C_Mask);   
-        }else {
+        if (value) {
+            setStatusBit(C_Mask);
+        } else {
             clearStatusBit(C_Mask);
         }
     }
@@ -66,16 +98,21 @@ public class DataMemory {
             clearStatusBit(DC_Mask);
         }
     }
-    
-    // --- FSR --- (Addr: 0x04 & 0x84) -------------------------------------------------
-    
-    // --- PCLATH --- (Addr: 0x0A & 0x8A) ----------------------------------------------
-    
-    // --- INTCON --- (Addr: 0x0B & 0x8B) ----------------------------------------------
-    
-    // --- General Purspose SRAM --- (Addr: 0x0C-0x4F & 0x8C-0xCF) ---------------------
-    
-    // --- undefined/"empty" --- (Addr: 0x50-0x7F & 0xD0-FF) ---------------------------
-    		//-> these can simply be set to 0 and mapped
+
+    // --- FSR --- (Addr: 0x04 & 0x84)
+    // -------------------------------------------------
+
+    // --- PCLATH --- (Addr: 0x0A & 0x8A)
+    // ----------------------------------------------
+
+    // --- INTCON --- (Addr: 0x0B & 0x8B)
+    // ----------------------------------------------
+
+    // --- General Purspose SRAM --- (Addr: 0x0C-0x4F & 0x8C-0xCF)
+    // ---------------------
+
+    // --- undefined/"empty" --- (Addr: 0x50-0x7F & 0xD0-FF)
+    // ---------------------------
+    // -> these can simply be set to 0 and mapped
 
 }
---
+++ PicSimulatorJava/src/simulatorCode/Port.java
@@ -0,0 +1,44 @@
+package simulatorCode;
+
+public class Port {
+    private int value;
+    private int tris;// 1=input 0=output
+    private int latch;
+
+    private final int width;
+    private final String name;
+
+    public Port(String name, int width) {
+        this.name = name;
+        this.width = width;
+        this.value = 0;
+        this.tris = 0xFF;
+        this.latch = 0;
+    }
+
+    public void setTris(int mask) {
+        this.tris = mask & getMaxBitMask();
+    }
+
+    public void write(int data) {
+        this.latch = data & getMaxBitMask();
+        this.value = (this.value & tris) | (latch & ~tris);
+    }
+
+    public int read() {
+        return (value & tris) | (latch & ~tris);
+    }
+
+    public int getTris() {
+        return tris;
+    }
+
+    public int getLatch() {
+        return latch;
+    }
+
+    private int getMaxBitMask() {
+        return (1 << width) - 1;
+    }
+
+}
